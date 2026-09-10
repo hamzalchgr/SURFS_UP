fe6131c3adb7d0714e99510712272296c3816bf8
hamzalchgr <hamzalchgrdev@gmail.com>
2026-09-11 01:31:00 +0200

    Connecting the server to the client side.

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+   {
+      name: 'server',
+      cwd: 'packages/server',
+      command: 'bun run dev',
+      prefixColor: 'yellow'
+   },
+   {
+      name: 'client',
+      cwd: 'packages/client',
+      command: 'bun run dev',
+      prefixColor: 'blue'
+   }
+])
